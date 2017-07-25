5527cd7aa48365c17adf174a68e1231b270fde0b
qilianshan [祁连山] <qilianshan@xiaomi.com>
2017-07-25 10:29:00 +0800

    add file
    Signed-off-by: qilianshan [祁连山] <qilianshan@xiaomi.com>

--- src/main/java/A_Introduction/simpleCompile.java
+++ src/main/java/A_Introduction/simpleCompile.java
@@ -3,6 +3,11 @@ package A_Introduction;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -11,22 +16,35 @@ import java.util.Stack;
 public class simpleCompile {
     public static void main(String[] args){
 
-        readFile("/Users/qilianshan/Documents/mySpace/javaExerciseSet/src/main/resources/compileExercise/main");
+        compile("/Users/qilianshan/Documents/mySpace/javaExerciseSet/src/main/resources/compileExercise/main"
+                , "/Users/qilianshan/Documents/mySpace/javaExerciseSet/src/main/resources/compileExercise/output");
     }
 
     public static Stack<String> pathNames=new Stack<String>();
-    public static void readFile(String pathName){
+
+    public static List<String> lines=new ArrayList<String>();
+
+    public static void compile(String start,String output){
+        readFile(start);
         try{
+            Files.write(Paths.get(output),lines, Charset.forName("UTF-8"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void readFile(String pathName){
+         try{
             BufferedReader br=new BufferedReader(new FileReader(pathName));
             for(String line;(line=br.readLine())!=null;){
                 if(line.contains("#include")){
-                    String subPath=line.split(" ")[1];
+                    String subPath= (Paths.get(Paths.get(pathName).getParent().toString(), line.split(" ")[1])).toString();
                     if (!pathNames.contains(subPath)) {
+                        pathNames.push(subPath);
                         readFile(subPath);
                     }
-                    pathNames.push(subPath);
+                }else{
+                    lines.add(line);
                 }
-                System.out.println(line);
             }
         }catch (Exception e){
             e.printStackTrace();
