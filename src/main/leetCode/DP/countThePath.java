08226f6c376c3acafe944d6663fe3156ad523f6d
qilianshan@git.n.xiaomi.com <qilianshan@git.n.xiaomi.com>
2018-01-03 15:37:29 +0800

    countThePath
    Signed-off-by: qilianshan@git.n.xiaomi.com <qilianshan@git.n.xiaomi.com>

---
+++ src/main/leetCode/DP/countThePath.java
@@ -0,0 +1,14 @@
+package DP;
+
+import java.util.List;
+
+/**
+ * Created by qilianshan on 18/1/3.
+ */
+public class countThePath {
+    public static int count_the_paths(List<String> grid) {
+        // Write your solution here
+        int[][] fn=new int[512][512];
+        return 1;
+    }
+}
